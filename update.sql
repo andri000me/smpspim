3b6392c777af7b69d67ccf8b92f33056c3f53f75
rohmad <rohmad.ew@gmail.com>
2017-08-13 12:16:15 +0700

    Fix form aplikasi Fix urutan pengaturan

---
+++ update.sql
@@ -0,0 +1,7 @@
+UPDATE `md_pengaturan` SET `ORDER_PENGATURAN` = '18' WHERE `md_pengaturan`.`ID_PENGATURAN` = 'ketua_p3h_banin';
+UPDATE `md_pengaturan` SET `ORDER_PENGATURAN` = '19' WHERE `md_pengaturan`.`ID_PENGATURAN` = 'ketua_p3h_banat';
+UPDATE `md_pengaturan` SET `ORDER_PENGATURAN` = '20' WHERE `md_pengaturan`.`ID_PENGATURAN` = 'ketua_komdis';
+UPDATE `md_pengaturan` SET `ORDER_PENGATURAN` = '21' WHERE `md_pengaturan`.`ID_PENGATURAN` = 'nilai_minimal_hafal';
+UPDATE `md_pengaturan` SET `ORDER_PENGATURAN` = '22' WHERE `md_pengaturan`.`ID_PENGATURAN` = 'maksimal_lari_hafalan';
+UPDATE `md_pengaturan` SET `ORDER_PENGATURAN` = '23' WHERE `md_pengaturan`.`ID_PENGATURAN` = 'lama_log_tersimpan';
+UPDATE `md_pengaturan` SET `NAMA_PENGATURAN` = '20' WHERE `md_pengaturan`.`ID_PENGATURAN` = 'ketua_p3h_banin';
