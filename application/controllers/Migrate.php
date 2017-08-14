bb4f83e94992146e3451275282c2e4675bf24573
rohmad <rohmad.ew@gmail.com>
2017-08-14 09:25:04 +0700

    Fix Update SQL untuk hakakses keuangan

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -1,12 +1,24 @@
 <?php
 
 class Migrate extends CI_Controller {
-
-    public function index() {
+    
+    public function __construct() {
+        parent::__construct();
+        
         $this->auth->validation();
         $this->load->library('migration');
+    }
 
+    public function index() {
         if ($this->migration->current() === FALSE) {
+            show_error($this->migration->error_string());
+        } else {
+            echo '<h1>Migration Success</h1>';
+        }
+    }
+    
+    public function version($version) {
+        if ($this->migration->version($version) === FALSE) {
             show_error($this->migration->error_string());
         } else {
             echo '<h1>Migration Success</h1>';
