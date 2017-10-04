ab42a1d9275f7ce0c1ed1f29646db281c71b7b22
rohmad <rohmad.ew@gmail.com>
2017-10-04 14:05:20 +0700

    BLANKO NILAI

--- application/views/backend/pu/jadwal_us/cetak_blanko_nilai.php
+++ application/views/backend/pu/jadwal_us/cetak_blanko_nilai.php
@@ -12,21 +12,26 @@ $pdf = $this->fpdf;
 
 $temp_kelas = NULL;
 $f4 = $this->pengaturan->getUkuranF4();
-$width = $f4[0]/2;
+$width = $f4[0] / 2;
 $height = $f4[1];
 
 foreach ($data as $detail) {
     if ($detail->ID_KELAS != $temp_kelas) {
         if ($temp_kelas != NULL) {
+            $pdf->Ln(3);
             $pdf->Cell(30);
-            $pdf->Cell(0, 10, 'Pengampu,');
+            $pdf->Cell(0, 5, '.......................,..............................');
+            $pdf->Ln();
+            $pdf->Cell(30);
+            $pdf->Cell(0, 10, 'Pengampu mapel,');
             $pdf->Ln(14);
             $pdf->Cell(30);
-            $pdf->Cell(0, 5, '...............................................');
+            $pdf->Cell(0, 5, '.............................');
         }
 
         $temp_kelas = $detail->ID_KELAS;
 
+        $pdf->SetLeftMargin(7);
         $pdf->AddPage("P", array($width, $height));
 
         $pdf->SetFont('Arial', 'B', 12);
@@ -40,27 +45,33 @@ foreach ($data as $detail) {
         $pdf->Cell(0, 5, 'WALI KELAS: ' . $this->cetak->nama_peg_print($detail));
         $pdf->Ln();
 
-        $pdf->Cell(0, 10, 'MAPEL: ...............................................');
-        $pdf->Ln(8);
+//        $pdf->Cell(0, 10, 'MAPEL: ...............................................');
+//        $pdf->Ln(8);
 
         $pdf->SetFont('Arial', 'B', 9);
         $pdf->Cell(7, 5, 'NO', 1, 0, 'C');
         $pdf->Cell(23, 5, 'NIS', 1, 0, 'C');
-        $pdf->Cell(45, 5, 'NAMA', 1, 0, 'C');
-        $pdf->Cell(15, 5, 'NILAI', 1, 0, 'C');
+        $pdf->Cell(40, 5, 'NAMA', 1, 0, 'C');
+        $pdf->Cell(12, 5, 'MAPEL', 1, 0, 'C');
+        $pdf->Cell(12, 5, 'MAPEL', 1, 0, 'C');
         $pdf->Ln();
 
         $pdf->SetFont('Arial', '', 9);
     }
 
-    $pdf->Cell(7, 5, $detail->NO_ABSEN_AS, 1);
-    $pdf->Cell(23, 5, $detail->NIS_SISWA, 1);
-    $pdf->Cell(45, 5, $this->pdf_handler->cut_text($pdf, $detail->NAMA_SISWA, 45), 1);
-    $pdf->Cell(15, 5, '', 1);
+    $pdf->Cell(7, 6, $detail->NO_ABSEN_AS, 1);
+    $pdf->Cell(23, 6, $detail->NIS_SISWA, 1);
+    $pdf->Cell(40, 6, $this->pdf_handler->cut_text($pdf, $detail->NAMA_SISWA, 40), 1);
+    $pdf->Cell(12, 6, '', 1);
+    $pdf->Cell(12, 6, '', 1);
     $pdf->Ln();
 }
+$pdf->Ln(3);
 $pdf->Cell(30);
-$pdf->Cell(0, 10, 'Pengampu,');
+$pdf->Cell(0, 5, '.......................,..............................');
+$pdf->Ln();
+$pdf->Cell(30);
+$pdf->Cell(0, 10, 'Pengampu mapel,');
 $pdf->Ln(14);
 $pdf->Cell(30);
 $pdf->Cell(0, 5, '.............................');
