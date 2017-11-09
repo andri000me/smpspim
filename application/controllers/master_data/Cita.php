1f2a9a31718bf0792725ab8090766b9babadc7b9
rohmad <rohmad.ew@gmail.com>
2017-11-09 20:32:56 +0700

    MODUL MASTER DATA ADD MENU CITA-CITA

---
+++ application/controllers/master_data/Cita.php
@@ -0,0 +1,136 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Sekolah
+ *
+ * @author rohmad
+ */
+class Cita extends CI_Controller {
+    
+    var $edit_id = TRUE;
+    var $primary_key = "ID_CITA";
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->model('cita_model', 'cita');
+        $this->auth->validation(array(11, 2));
+    }
+
+    public function index() {
+        $this->generate->backend_view('master_data/cita/index');
+    }
+
+    public function ajax_list() {
+        $this->generate->set_header_JSON();
+
+        $id_datatables = 'datatable1';
+        $list = $this->cita->get_datatables();
+        $data = array();
+        $no = $_POST['start'];
+        foreach ($list as $item) {
+            $no++;
+            $row = array();
+            $row[] = $item->ID_CITA;
+            $row[] = $item->NAMA_CITA;
+
+            $row[] = '
+                <div class="btn-group">
+                    <button data-toggle="dropdown" class="btn btn-primary btn-xs dropdown-toggle" aria-expanded="false">AKSI&nbsp;&nbsp;<span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="javascript:void()" title="Ubah" onclick="update_data_' . $id_datatables . '(\'' . $item->ID_CITA . '\')"><i class="fa fa-pencil"></i>&nbsp;&nbsp;Ubah</a></li>
+                        <li><a href="javascript:void()" title="Hapus" onclick="delete_data_' . $id_datatables . '(\'' . $item->ID_CITA . '\')"><i class="fa fa-trash"></i>&nbsp;&nbsp;Hapus</a></li>
+                    </ul>
+                </div>';
+
+            $data[] = $row;
+        }
+
+        $output = array(
+            "draw" => $_POST['draw'],
+            "recordsTotal" => $this->cita->count_all(),
+            "recordsFiltered" => $this->cita->count_filtered(),
+            "data" => $data,
+        );
+
+        $this->generate->output_JSON($output);
+    }
+
+    public function request_form() {
+        $data = $this->generate->set_header_form_JSON($this->cita);
+        
+        $input_id = FALSE;
+        $show_id = TRUE;
+
+        $data_html = array(
+            array(
+                'label' => 'Nama',
+                'required' => TRUE,
+                'keterangan' => 'Wajib diisi',
+                'length' => 7,
+                'data' => array(
+                    'type' => 'text',
+                    'name' => 'NAMA_CITA',
+                    "placeholder" => " ",
+                    'value' => $data == NULL ? "" : $data->NAMA_CITA
+                )
+            )
+        );
+        
+        $this->generate->output_form_JSON($data, $this->primary_key, $data_html, $input_id, $show_id, $this->edit_id);
+    }
+
+    public function ajax_add() {
+        $this->generate->set_header_JSON();
+        $this->generate->cek_validation_form('add');
+
+        $data = array(
+            'NAMA_CITA' => $this->input->post('NAMA_CITA')
+        );
+        $insert = $this->cita->save($data);
+
+        $this->generate->output_JSON(array("status" => $insert));
+    }
+
+    public function ajax_update() {
+        $this->generate->set_header_JSON();
+        $this->generate->cek_validation_form('edit');
+        $cek = $this->generate->cek_update_id($this->edit_id, $this->primary_key, $this->input->post());
+        
+        $where = $cek['where'];
+        
+        if (isset($cek['data'])) $data = $cek['data'];
+        else $data = array();
+        
+        $data['NAMA_CITA'] = $this->input->post('NAMA_CITA');
+        
+        $affected_row = $this->cita->update($where, $data);
+
+        $this->generate->output_JSON(array("status" => $affected_row));
+    }
+
+    public function ajax_delete() {
+        $this->generate->set_header_JSON();
+        $this->generate->cek_validation_form('delete');
+
+        $id = $this->input->post("ID");
+        $affected_row = $this->cita->delete_by_id($id);
+
+        $this->generate->output_JSON(array("status" => $affected_row));
+    }
+
+    public function auto_complete() {
+        $this->generate->set_header_JSON();
+        
+        $data = $this->cita->get_all_ac($this->input->post('q'));
+        
+        $this->generate->output_JSON($data);
+    }
+
+}
--- update.sql
+++ update.sql
@@ -29,3 +29,29 @@ ALTER TABLE `md_siswa` CHANGE `STATUS_KK_SISWA` `STATUS_KK_SISWA` INT(12) NULL D
 INSERT INTO `md_hobi` (`ID_HOBI`, `NAMA_HOBI`) VALUES (NULL, 'Olahraga'), (NULL, 'Kesenian'), (NULL, 'Membaca'), (NULL, 'Menulis'), (NULL, 'Travelling'), (NULL, 'Lainnya');
 INSERT INTO `md_menu` (`ID_MENU`, `NAME_MENU`, `CONTROLLER_MENU`, `FUNCTION_MENU`, `ICON_MENU`, `SHOW_MENU`, `LEVEL_CHILD`, `HAVE_CHILD`) VALUES ('01103012000', 'HOBI', 'master_data/hobi', 'index', NULL, '1', '2', '0'), ('01103012001', 'HOBI - ADD', 'master_data/hobi', 'add', NULL, '0', '2', '0'), ('01103012002', 'HOBI - EDIT', 'master_data/hobi', 'edit', NULL, '0', '2', '0'), ('01103012003', 'HOBI - DELETE', 'master_data/hobi', 'delete', NULL, '0', '2', '0');
 INSERT INTO `md_levelmenu` (`MENU_LEVELMENU`, `HAKAKSES_LEVELMENU`) VALUES ('01103012000', '11'), ('01103012001', '11'), ('01103012002', '11'), ('01103012003', '11');
+
+
+CREATE TABLE `md_cita` (
+  `ID_CITA` int(12) NOT NULL,
+  `NAMA_CITA` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `md_cita` (`ID_CITA`, `NAMA_CITA`) VALUES
+(1, 'PNS'),
+(2, 'TNI/POLRI'),
+(3, 'Guru/Dosen'),
+(4, 'Dokter'),
+(5, 'Politikus'),
+(6, 'Wiraswasta'),
+(7, 'Pekerja Senu/Lukis/Artis/Sejenis'),
+(8, 'Lainya');
+ALTER TABLE `md_cita`
+  ADD PRIMARY KEY (`ID_CITA`);
+ALTER TABLE `md_cita`
+  MODIFY `ID_CITA` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `md_siswa` ADD `CITA_SISWA` INT(12) NOT NULL AFTER `HOBI_SISWA`, ADD INDEX (`CITA_SISWA`);
+ALTER TABLE `md_siswa` CHANGE `CITA_SISWA` `CITA_SISWA` INT(12) NULL DEFAULT NULL;
+UPDATE md_siswa SET CITA_SISWA=NULL;
+ALTER TABLE `md_siswa` ADD FOREIGN KEY (`CITA_SISWA`) REFERENCES `simapes_9`.`md_cita`(`ID_CITA`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+INSERT INTO `md_menu` (`ID_MENU`, `NAME_MENU`, `CONTROLLER_MENU`, `FUNCTION_MENU`, `ICON_MENU`, `SHOW_MENU`, `LEVEL_CHILD`, `HAVE_CHILD`) VALUES ('01103013000', 'CITA', 'master_data/cita', 'index', NULL, '1', '2', '0'), ('01103013001', 'CITA - ADD', 'master_data/cita', 'add', NULL, '0', '2', '0'), ('01103013002', 'CITA - EDIT', 'master_data/cita', 'edit', NULL, '0', '2', '0'), ('01103013003', 'CITA - DELETE', 'master_data/cita', 'delete', NULL, '0', '2', '0');
+INSERT INTO `md_levelmenu` (`MENU_LEVELMENU`, `HAKAKSES_LEVELMENU`) VALUES ('01103013000', '11'), ('01103013001', '11'), ('01103013002', '11'), ('01103013003', '11');
