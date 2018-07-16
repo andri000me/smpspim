ee4f8a8ec874e9944aab3afc429bd0447653c53b
Moch Syams El Maarif <Moch Syams El Maarif@TU-PIM>
2018-07-16 15:54:21 +0700

    UPDATE Cetak Keterangan Pindah

--- application/views/backend/akademik/mutasi/cetak.php
+++ application/views/backend/akademik/mutasi/cetak.php
@@ -21,96 +21,88 @@ $alasan = array(
     99 => 'karena telah lulus',
 );
 
-for ($i = 0; $i < 2; $i++) {
-    $pdf->SetLeftMargin(15);
-    $pdf->SetRightMargin(15);
+    $pdf->SetLeftMargin(25);
+    $pdf->SetRightMargin(25);
+    $pdf->SetTopMargin(45);
     $pdf->AddPage("P", $this->pengaturan->getUkuranF4());
     $pdf->SetAutoPageBreak(true, 0);
 
-    $pdf = $this->cetak->header_yayasan($pdf, 0, 'f4');
-
     $pdf->Ln(7);
-    $pdf->SetFont('Arial', 'B', 14);
-    $pdf->Cell(0, 5, 'SURAT KETERANGAN', 0, 0, 'C');
+    $pdf->SetFont('Times', 'BU', 16);
+    $pdf->Cell(0, 7, 'SURAT KETERANGAN PINDAH', 0, 0, 'C');
     $pdf->Ln();
 
-    $pdf->SetLineWidth(0.30);
-    $pdf->Line(75, 48, 135, 48);
-
-    $pdf->SetFont('Arial', '', 12);
-    $pdf->Cell(0, 5, 'Nomor: KM/         /A-II/PIM/' . (date('Y') - $this->pengaturan->getTahunBerdiri()) . '/' . $this->date_format->toRomawi(date('n')) . '/' . date('Y'), 0, 0, 'C');
+    $pdf->SetFont('Times', '', 12);
+    $pdf->Cell(0, 7, 'Nomor: KM/      /A-II/PIM/' . (date('Y') - $this->pengaturan->getTahunBerdiri()) . '/' . $this->date_format->toRomawi(date('n')) . '/' . date('Y'), 0, 0, 'C');
     $pdf->Ln(10);
 
-    $pdf->MultiCell(0, 5, 'Direktur Perguruan Islam Mathali\'ul Falah Kajen Margoyoso Pati, menerangkan dengan sesungguhnya bahwa :');
+    $pdf->MultiCell(0, 7, 'Direktur Perguruan Islam Mathali\'ul Falah Kajen Margoyoso Pati, menerangkan dengan sesungguhnya bahwa :');
     $pdf->Ln();
 
     $pdf->Cell(20);
-    $pdf->Cell(20, 5, 'NIS');
-    $pdf->Cell(0, 5, ': ' . $siswa->NIS_NIS);
+    $pdf->Cell(20, 7, 'No. Induk');
+    $pdf->Cell(0, 7, ': ' . $siswa->NIS_NIS);
     $pdf->Ln();
 
     $pdf->Cell(20);
-    $pdf->Cell(20, 5, 'Nama');
-    $pdf->Cell(0, 5, ': ' . $siswa->NAMA_SISWA);
+    $pdf->Cell(20, 7, 'Nama');
+    $pdf->Cell(0, 7, ': ' . $siswa->NAMA_SISWA);
     $pdf->Ln();
 
     $pdf->Cell(20);
-    $pdf->Cell(20, 5, 'TTL');
-    $pdf->Cell(0, 5, ': ' . $siswa->TEMPAT_LAHIR_SISWA . ', ' . $this->date_format->to_print_text($siswa->TANGGAL_LAHIR_SISWA));
+    $pdf->Cell(20, 7, 'TTL');
+    $pdf->Cell(0, 7, ': ' . $siswa->TEMPAT_LAHIR_SISWA . ', ' . $this->date_format->to_print_text($siswa->TANGGAL_LAHIR_SISWA));
     $pdf->Ln();
 
     $pdf->Cell(20);
-    $pdf->Cell(20, 5, 'Orang tua');
-    $pdf->Cell(0, 5, ': ' . $siswa->AYAH_NAMA_SISWA);
+    $pdf->Cell(20, 7, 'Orang tua');
+    $pdf->Cell(0, 7, ': ' . $siswa->AYAH_NAMA_SISWA);
     $pdf->Ln();
 
     $pdf->Cell(20);
-    $pdf->Cell(20, 5, 'Alamat');
-    $pdf->Cell(0, 5, ': ' . $siswa->ALAMAT_SISWA);
+    $pdf->Cell(20, 7, 'Alamat');
+    $pdf->Cell(0, 7, ': ' . $siswa->ALAMAT_SISWA);
     $pdf->Ln();
 
     $pdf->Cell(42);
-    $pdf->Cell(0, 5, 'Kec. ' . $siswa->NAMA_KEC . ' ' . $siswa->NAMA_KAB . ' ' . $siswa->NAMA_PROV);
+    $pdf->Cell(0, 7, 'Kec. ' . $siswa->NAMA_KEC . ', ' . $siswa->NAMA_KAB . ', ' . $siswa->NAMA_PROV);
     $pdf->Ln();
 
     $pdf->Cell(20);
-    $pdf->Cell(20, 5, 'Kelas');
-    $pdf->Cell(0, 5, ': ' . ($siswa->NAMA_KELAS == NULL ? '-' : $siswa->NAMA_KELAS));
+    $pdf->Cell(20, 7, 'Kelas');
+    $pdf->Cell(0, 7, ': ' . ($siswa->NAMA_KELAS == NULL ? '-' : $siswa->NAMA_KELAS));
     $pdf->Ln(10);
 
-    $pdf->MultiCell(0, 5, 'adalah benar-benar belajar ' . ($siswa->NAMA_TINGK == NULL ? 'pelajar di' : ('di kelas ' . $siswa->NAMA_TINGK . ' (' . trim($this->money->terbilang($siswa->NAMA_TINGK)) . ') ' . ucwords(strtolower($siswa->NAMA_DEPT)))) . ' Perguruan Islam Mathali\'ul Falah Kajen Margoyoso Pati Jawa Tengah pada tahun ajaran ' . $this->session->userdata('NAMA_TA_ACTIVE') . '.');
+    $pdf->MultiCell(0, 7, 'adalah benar-benar belajar ' . ($siswa->NAMA_TINGK == NULL ? 'pelajar di' : ('di kelas ' . $siswa->NAMA_TINGK . ' (' . trim($this->money->terbilang($siswa->NAMA_TINGK)) . ') ' . ucwords(strtolower($siswa->NAMA_DEPT)))) . ' Perguruan Islam Mathali\'ul Falah Kajen Margoyoso Pati Jawa Tengah pada tahun ajaran ' . $this->session->userdata('NAMA_TA_ACTIVE') . '.');
     $pdf->Ln();
 
-    $pdf->MultiCell(0, 5, 'Siswa tersebut keluar sekolah pada tanggal ' . $this->date_format->to_print_text($siswa->TANGGAL_MUTASI_SISWA) . ' ' . (isset($alasan[$siswa->ID_MUTASI]) ? $alasan[$siswa->ID_MUTASI] : 'atas permintaan sendiri dan orang tua karena pindah sekolah') . '.');
+    $pdf->MultiCell(0, 7, 'Siswa tersebut keluar sekolah pada tanggal ' . $this->date_format->to_print_text($siswa->TANGGAL_MUTASI_SISWA) . ' ' . (isset($alasan[$siswa->ID_MUTASI]) ? $alasan[$siswa->ID_MUTASI] : 'atas permintaan sendiri dan orang tua karena pindah sekolah') . '.');
     $pdf->Ln();
 
-    $pdf->MultiCell(0, 5, 'Demikian surat ini dibuat untuk menjadikan maklum adanya.');
+    $pdf->MultiCell(0, 7, 'Demikian surat ini dibuat untuk menjadikan maklum adanya.');
     $pdf->Ln(15);
 
-    $pdf->Cell(100);
-    $pdf->Cell(0, 5, 'Kajen, ' . $this->date_format->to_print_text(date('Y-m-d')));
+    $pdf->Cell(90);
+    $pdf->Cell(0, 7, 'Kajen, ' . $this->date_format->to_print_text(date('Y-m-d')));
     $pdf->Ln(7);
 
-    $pdf->Cell(100);
-    $pdf->Cell(0, 5, 'An. Direktur,');
+    $pdf->Cell(90);
+    $pdf->Cell(0, 7, 'An. Direktur,');
     $pdf->Ln();
 
-    $pdf->Cell(100);
-    $pdf->Cell(0, 5, 'Pembantu Direktur Bidang');
+    $pdf->Cell(90);
+    $pdf->Cell(0, 7, 'Pembantu Direktur Bidang');
     $pdf->Ln();
 
-    $pdf->Cell(100);
-    $pdf->Cell(0, 5, 'Ketata Usahaan Dan Keuangan');
-    $pdf->Ln(20);
+    $pdf->Cell(90);
+    $pdf->Cell(0, 7, 'Ketata Usahaan Dan Keuangan');
+    $pdf->Ln(30);
 
-    $pdf->SetFont('Arial', 'UB', 12);
+    $pdf->Image(base_url('files/aplikasi/ttd_wahib.png'), 105, 230, 60);
+    $pdf->SetFont('Times', 'UB', 12);
 
-    $pdf->Cell(100);
-    if ($i == 0)
-        $pdf->Cell(0, 5, 'H. Muhammad Ulin Nuha, Lc.');
-    elseif ($i == 1)
-        $pdf->Cell(0, 5, 'H. Mohammad Wahib Asnawi, Lc.');
+    $pdf->Cell(90);
+    $pdf->Cell(0, 7, $this->cetak->nama_peg_print($this->pengaturan->getPDTUKeuangan2(), FALSE));
     $pdf->Ln();
-}
 
 $pdf->Output();
