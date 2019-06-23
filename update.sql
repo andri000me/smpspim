04cb0369236a6e5b9b17a55465edf1d1331920da
rohmad <rohmad.ew@gmail.com>
2019-06-23 22:30:26 +0700

    UM tingkat MI hanya kelas 2 dan 3 UM tingkat TS hanya kelas 1 dan 2 UM tingkat AL hanya kelas 1 UM tingkat DU dan DW hanya kelas 2

--- update.sql
+++ update.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `simapes`.`md_siswa` 
 ADD COLUMN `KIP_SISWA` VARCHAR(10) NULL DEFAULT NULL AFTER `CITA_SISWA`;
+
+UPDATE `simapes`.`md_pengaturan` SET `NAMA_PENGATURAN` = '{\"2\":[2,3],\"3\":[1,2],\"4\":[1],\"5\":[2],\"6\":[2]}' WHERE (`ID_PENGATURAN` = 'psb_ujian');
