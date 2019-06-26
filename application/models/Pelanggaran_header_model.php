7532672e610ccd5d271af2a35adb6aca43a3934a
Moch Syams El Maarif <Moch Syams El Maarif@TU-PIM>
2019-06-26 08:13:11 +0700

    Update lanjut jenjang

--- application/models/Pelanggaran_header_model.php
+++ application/models/Pelanggaran_header_model.php
@@ -203,7 +203,7 @@ class Pelanggaran_header_model extends CI_Model {
         return $this->db->count_all_results();
     }
 
-    public function save_lanjut_jenjang($data) {
+    public function create_row($data) {
         $check = array(
             'TA_KSH' => $data['TA_KSH'],
             'CAWU_KSH' => $data['CAWU_KSH'],
