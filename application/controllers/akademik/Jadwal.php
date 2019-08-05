bedbcb0fc893cdf2a34b2c112ac17a4259847950
rohmad <rohmad.ew@gmail.com>
2019-08-05 14:45:45 +0700

    Cetak Jadwal Guru Kelas

--- application/controllers/akademik/Jadwal.php
+++ application/controllers/akademik/Jadwal.php
@@ -145,7 +145,7 @@ class Jadwal extends CI_Controller {
             'jadwal' => array(),
         );
 
-        foreach ($kelas as $detail_kelas) {
+        foreach ($kelas as $index => $detail_kelas) {
             foreach ($hari as $detail_hari) {
                 $data_jp = array(
                     'DEPT_MJP' => $detail_kelas->ID_DEPT,
@@ -153,14 +153,13 @@ class Jadwal extends CI_Controller {
                 );
                 $data['jam_pelajaran'][$detail_kelas->ID_KELAS][$detail_hari->ID_HARI] = $this->jp->get_rows($data_jp);
                 foreach ($data['jam_pelajaran'][$detail_kelas->ID_KELAS][$detail_hari->ID_HARI] as $detail_jp) {
-                    $result = $this->jadwal->get_jadwal_kelas($detail_kelas->ID_KELAS, $detail_hari->ID_HARI, $detail_jp->ID_MJP);
+                    $result = $this->jadwal->get_jadwal_kelas($detail_kelas->ID_KELAS, $detail_hari->ID_HARI, $detail_jp->URUTAN_MJP);
                     if (count($result) > 0)
-                        $data['jadwal'][$detail_kelas->ID_KELAS][$detail_hari->ID_HARI][$detail_jp->ID_MJP] = $result;
+                        $data['jadwal'][$detail_kelas->ID_KELAS][$detail_hari->ID_HARI][$detail_jp->URUTAN_MJP] = $result;
                 }
             }
-            
-            break;
         }
+        
         
         $this->load->view('backend/akademik/jadwal/cetak_jadwal_kelas', $data);
     }
--- application/views/backend/akademik/jadwal/cetak_jadwal_kelas.php
+++ application/views/backend/akademik/jadwal/cetak_jadwal_kelas.php
@@ -7,13 +7,12 @@ $JK = array(
     'P' => 'BANAT',
 );
 
-foreach ($kelas as $detail_kelas) {
-
+foreach ($kelas as $index => $detail_kelas) {
     $pdf->AddPage("L", "A4");
     $pdf->SetAutoPageBreak(true, 0);
 
     $pdf->SetFont('Arial', 'B', 12);
-    $pdf->Cell(0, 5, 'JADWAL PELAJARAN KELAS ' . $detail_kelas->NAMA_KELAS . ' ' . $JK[$detail_kelas->JK_KELAS], 0, 0, 'C');
+    $pdf->Cell(0, 5, 'JADWAL PELAJARAN KELAS ' . strtoupper($detail_kelas->NAMA_KELAS), 0, 0, 'C');
     $pdf->Ln();
 
     $pdf->Cell(0, 5, 'PERGURUAN ISLAM MATHALI\'UL FALAH', 0, 0, 'C');
@@ -23,16 +22,23 @@ foreach ($kelas as $detail_kelas) {
     $pdf->Ln(10);
 
     $pdf->SetFont('Arial', 'B', 10);
-    $pdf->Cell(35, 5, 'Jam', 1, 0, 'C');
+    $pdf->Cell(35, 6, 'Jam', 1, 0, 'C');
     foreach ($hari as $detail_hari) {
-        $pdf->Cell(40, 5, $detail_hari->NAMA_HARI, 1, 0, 'C');
+        $pdf->Cell(40, 6, $detail_hari->NAMA_HARI, 1, 0, 'C');
     }
     $pdf->Ln();
 
-    $pdf->SetFont('Arial', '', 9);
     foreach ($jam_pelajaran[$detail_kelas->ID_KELAS][$detail_hari->ID_HARI] as $detail_jp) {
-        $data_jadwal = array();
-        $data_jadwal[] = array('align' => 'C', 'width' => 35, 'height' => 8, 'text' => $detail_jp->MULAI_MJP . ' - ' . $detail_jp->AKHIR_MJP);
+        $pdf->SetFont('Arial', 'B', 9);
+        $pdf->Cell(35, 14, $detail_jp->MULAI_MJP . " - " . $detail_jp->AKHIR_MJP, 1, 0, 'C');
+       
+        foreach ($hari as $detail_hari) {
+            $pdf->Cell(40, 2, "", 'TLR', 0, 'L');
+        }
+        $pdf->Ln();
+        
+        $pdf->SetFont('Arial', '', 9);
+        $pdf->Cell(35);
         foreach ($hari as $detail_hari) {
             if (isset($jadwal[$detail_kelas->ID_KELAS][$detail_hari->ID_HARI][$detail_jp->ID_MJP])) {
                 $jadwal_kelas = $jadwal[$detail_kelas->ID_KELAS][$detail_hari->ID_HARI][$detail_jp->ID_MJP];
@@ -40,15 +46,54 @@ foreach ($kelas as $detail_kelas) {
                 foreach ($jadwal_kelas as $detail_jadwal) {
                     $mapel .= $detail_jadwal->NAMA_MAPEL;
                 }
-                $data_jadwal[] = array('align' => 'L', 'width' => 40, 'height' => 8,'text' => $mapel);
+                $pdf->Cell(40, 6, $mapel, 'LR', 0, 'L');
             } else {
-                $data_jadwal[] = array('align' => 'C', 'width' => 40, 'height' => 8, 'text' => '-');
+                $pdf->Cell(40, 6, "-", 'LR', 0, 'C');
             }
         }
-        $pdf = $this->pdf_handler->wrap_row_table($pdf, $data_jadwal);
-    }
+        $pdf->Ln();
 
-    break;
+        $pdf->SetFont('Arial', 'I', 7);
+        $pdf->Cell(35);
+        foreach ($hari as $detail_hari) {
+            if (isset($jadwal[$detail_kelas->ID_KELAS][$detail_hari->ID_HARI][$detail_jp->ID_MJP])) {
+                $jadwal_kelas = $jadwal[$detail_kelas->ID_KELAS][$detail_hari->ID_HARI][$detail_jp->ID_MJP];
+                $mapel = '';
+                foreach ($jadwal_kelas as $detail_jadwal) {
+                    $mapel .= $this->cetak->nama_peg_print($detail_jadwal);
+                }
+                $pdf->Cell(40, 4, $mapel, 'LR', 0, 'L');
+            } else {
+                $pdf->Cell(40, 4, "-", 'LR', 0, 'C');
+            }
+        }
+        $pdf->Ln();
+        
+        $pdf->Cell(35);
+        foreach ($hari as $detail_hari) {
+            $pdf->Cell(40, 2, "", 'BLR', 0, 'L');
+        }
+        $pdf->Ln();
+//        $pdf = $this->pdf_handler->wrap_row_table($pdf, $data_jadwal);
+    }
+//    $pdf->SetFont('Arial', '', 9);
+//    foreach ($jam_pelajaran[$detail_kelas->ID_KELAS][$detail_hari->ID_HARI] as $detail_jp) {
+//        $data_jadwal = array();
+//        $data_jadwal[] = array('align' => 'C', 'width' => 35, 'height' => 8, 'text' => $detail_jp->MULAI_MJP . ' - ' . $detail_jp->AKHIR_MJP);
+//        foreach ($hari as $detail_hari) {
+//            if (isset($jadwal[$detail_kelas->ID_KELAS][$detail_hari->ID_HARI][$detail_jp->ID_MJP])) {
+//                $jadwal_kelas = $jadwal[$detail_kelas->ID_KELAS][$detail_hari->ID_HARI][$detail_jp->ID_MJP];
+//                $mapel = '';
+//                foreach ($jadwal_kelas as $detail_jadwal) {
+//                    $mapel .= $detail_jadwal->NAMA_MAPEL;
+//                }
+//                $data_jadwal[] = array('align' => 'L', 'width' => 40, 'height' => 8,'text' => $mapel);
+//            } else {
+//                $data_jadwal[] = array('align' => 'C', 'width' => 40, 'height' => 8, 'text' => '-');
+//            }
+//        }
+//        $pdf = $this->pdf_handler->wrap_row_table($pdf, $data_jadwal);
+//    }
 }
 
 $pdf->Output();
